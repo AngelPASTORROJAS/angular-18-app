99c853c060ce9f5088a5df14261770f1118ac6b2
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2024-08-08 00:23:34 +0200

    feat: utiliser un composant dans l'appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from "./user/user.component";
 
 @Component({                  // decorateur de la class AppComponent
   selector: 'app-root',       // le nom de la balise composant
   standalone: true,           // import de composant ou module extérieur est activer et se retrouve dans imports
-  imports: [RouterOutlet],
-  template: `<h1>Hello world in {{city.toUpperCase()}} {{ 1+1 }} </h1>`,
+  imports: [RouterOutlet, UserComponent],
+  template: `<h1>Hello world in {{city.toUpperCase()}} {{ 1+1 }} </h1>
+    <app-user></app-user>
+  `,
   styles: `
   h1 {
     color : red;
